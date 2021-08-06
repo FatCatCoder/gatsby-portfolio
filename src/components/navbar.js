2d87189ddd7b3241d7c1ef7bd1f69442399b551b
FatCatCoder <christian75armand@gmail.com>
2021-08-06 17:13:50 -0400

    particles

---
+++ src/components/navbar.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import { Link } from "gatsby"
+
+function Navbar(){
+    return(
+        <nav className="navbar navbar-expand-lg bg-transparent position-fixed start-50 translate-middle-x top-0">
+            <div className="container-fluid">
+                <a className="navbar-brand" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <Link className="nav-link active" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link active" to="/about">About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link active" to="/blog">Blog</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className="nav-link active" to="/contact">Contact</Link>
+                    </li>   
+                </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
