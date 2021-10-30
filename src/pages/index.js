d263b3a3fb8a1d6c88fe006f6dbeca70ad52c7db
FatCatCoder <christian75armand@gmail.com>
2021-10-29 22:26:34 -0400

    fixed rendering on macOS

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,32 +20,16 @@ import SmoothScrollBar from "../testing/SmoothScrollBar";
 import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 
-
-const IndexPage = () => {
-  // if (typeof window !== "undefined") {
-  //   gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
-  // }
-
-  function nativeSmoothScroll() {
-    const userPlatform = window?.navigator?.userAgentData?.platform || window?.navigator?.platform || 'disable-smooth-scroll';
-    const nativeSmoothScrollPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K', 'iPhone', 'iPad', 'iPod'];
-  
-    return nativeSmoothScrollPlatforms.includes(userPlatform)
-  }
-
+function NonNative(){
   const myContent = React.useRef();
-  React.useEffect(() => {
-    if(!nativeSmoothScroll()){
-      // SmoothScroll(myContent.current)
-    }
-  }, [])
-
-  // <SmoothScrollBar></SmoothScrollBar>
-
-  return (
+  let scrollOptions = {smooth: true, lerp: 0.05, multiplier: 1, containerRef: myContent, tablet: {smooth: false}, smartphone: {smooth: false}};
+  React.useEffect(()=>{
+    console.log('ok')
+  })
+  return(
     <>
+    <LocomotiveScrollProvider options={scrollOptions}>
     <Navbar />
-    <LocomotiveScrollProvider options={{smooth: true, lerp: 0.05, multiplier: 1, containerRef: myContent, tablet: {smooth: false}, smartphone: {smooth: false}}}>
     <div ref={myContent} data-scroll-container className="container-fluid">
       <Layout />
     </div>
@@ -53,6 +37,39 @@ const IndexPage = () => {
     </LocomotiveScrollProvider>
     </>
   )
+}
+
+function Native(){
+  return(
+    <>
+      <Navbar />
+      <div className="container-fluid">
+        <Layout />
+      </div>
+      <Particles className="vh-100 particles z-behind position-fixed top-0 p-0 m-0" params={particlesConfig}/>
+    </>
+  )
+}
+
+const IndexPage = () => {
+  // if (typeof window !== "undefined") {
+  //   gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+  // }
+  let [loaded, setLoaded] = React.useState(null);
+
+  function nativeSmoothScroll() {
+    const userPlatform = window?.navigator?.userAgentData?.platform || window?.navigator?.platform || 'disable-smooth-scroll';
+    const nativeSmoothScrollPlatforms = ['Macintosh', 'macOS', 'MacIntel', 'MacPPC', 'Mac68K', 'iPhone', 'iPad', 'iPod'];
+  
+    return nativeSmoothScrollPlatforms.includes(userPlatform)
+  }
+
+
+  React.useEffect(() => {
+    console.log('Refresh')
+  }, [])
+
+  return nativeSmoothScroll()? <Native /> : <NonNative />
 }
 
 export default IndexPage;
