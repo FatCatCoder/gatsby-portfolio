949b86cdcc273d1cec15c2c197d6c09db566826e
FatCatCoder <christian75armand@gmail.com>
2021-10-29 23:16:57 -0400

    Light/Dark mode toggle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,28 @@
 // plugins
 import * as React from "react"
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 import { gsap } from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 // components
 import Layout from '../components/Layout';
-import SmoothScroll from '../components/SmoothScroll';
 
 // styles
 import Particles from 'react-particles-js';
 import particlesConfig from "../styles/particlesConfig.json"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faSun, faMoon} from "@fortawesome/free-solid-svg-icons";
+import * as DarkTheme from "../styles/darkMode.module.scss" 
 import "../styles/global.scss"
+
 
 import Navbar from '../components/Navbar.jsx';
 import Sidebar from "../components/sidebar.js";
 
-import SmoothScrollBar from "../testing/SmoothScrollBar";
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 
-function NonNative(){
+function NonNative({theme}){
   const myContent = React.useRef();
   let scrollOptions = {smooth: true, lerp: 0.05, multiplier: 1, containerRef: myContent, tablet: {smooth: false}, smartphone: {smooth: false}};
   React.useEffect(()=>{
@@ -33,13 +35,13 @@ function NonNative(){
     <div ref={myContent} data-scroll-container className="container-fluid">
       <Layout />
     </div>
-    <Particles className="vh-100 particles z-behind position-fixed top-0 p-0 m-0" params={particlesConfig}/>
+    <Particles className={`vh-100 particles z-behind position-fixed top-0 p-0 m-0 ${theme || DarkTheme.particles}`} params={particlesConfig}/>
     </LocomotiveScrollProvider>
     </>
   )
 }
 
-function Native(){
+function Native({theme}){
   return(
     <>
       <Navbar />
@@ -51,50 +53,29 @@ function Native(){
   )
 }
 
+function nativeSmoothScroll() {
+  const userPlatform = window?.navigator?.userAgentData?.platform || window?.navigator?.platform || 'disable-smooth-scroll';
+  const nativeSmoothScrollPlatforms = ['Macintosh', 'macOS', 'MacIntel', 'MacPPC', 'Mac68K', 'iPhone', 'iPad', 'iPod'];
+  return nativeSmoothScrollPlatforms.includes(userPlatform)
+}
+
 const IndexPage = () => {
   // if (typeof window !== "undefined") {
   //   gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
   // }
-  let [loaded, setLoaded] = React.useState(null);
 
-  function nativeSmoothScroll() {
-    const userPlatform = window?.navigator?.userAgentData?.platform || window?.navigator?.platform || 'disable-smooth-scroll';
-    const nativeSmoothScrollPlatforms = ['Macintosh', 'macOS', 'MacIntel', 'MacPPC', 'Mac68K', 'iPhone', 'iPad', 'iPod'];
-  
-    return nativeSmoothScrollPlatforms.includes(userPlatform)
-  }
-
+  let [theme, setTheme] = React.useState(true); // True(light) / False(dark)
 
   React.useEffect(() => {
     console.log('Refresh')
   }, [])
 
-  return nativeSmoothScroll()? <Native /> : <NonNative />
+  return(
+    <>
+    {nativeSmoothScroll()? <Native theme={theme}/> : <NonNative theme={theme}/> }
+    <button className={`btn btn-transparent fs-4 position-fixed top-0 left-0 ${theme? 'text-warning':'text-light'}`} onClick={() => setTheme(!theme)}>Theme: {theme? <FontAwesomeIcon icon={faSun} />: <FontAwesomeIcon icon={faMoon} />}</button>
+    </>
+  )
 }
 
 export default IndexPage;
-
-
-/*
-
-  {!nativeSmoothScroll()?
-        <>
-      <Navbar />
-      <div ref={myContent} className="container-fluid">
-        <Layout />
-      </div>
-      <Particles className="vh-100 particles z-behind position-fixed top-0 p-0 m-0" params={particlesConfig}/>
-      </>
-      
-      :
-        <>
-      <Navbar />
-      <Sidebar />
-      <div ref={myContent} className="container-fluid">
-        <Layout />
-      </div>
-      <Particles className="vh-100 particles z-behind position-fixed top-0 p-0 m-0" params={particlesConfig}/>
-      </>
-    }
-
-*/
