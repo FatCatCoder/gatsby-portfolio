44a3e04c2cba179f3e6393c386cf1c4b8064b9b1
FatCatCoder <christian75armand@gmail.com>
2022-09-18 21:11:50 -0400

    fixed deploy

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,28 +1,28 @@
-const { createFilePath } = require(`gatsby-source-filesystem`)
+// const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    })
-  }
-}
+// exports.onCreateNode = ({ node, getNode, actions }) => {
+//   const { createNodeField } = actions
+//   if (node.internal.type === `MarkdownRemark`) {
+//     const slug = createFilePath({ node, getNode, basePath: `pages` })
+//     createNodeField({
+//       node,
+//       name: `slug`,
+//       value: slug,
+//     })
+//   }
+// }
 
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-    if (stage === "build-html") {
-      actions.setWebpackConfig({
-        module: {
-          rules: [
-            {
-              test: /locomotive-scroll/,
-              use: loaders.null(),
-            },
-          ],
-        },
-      })
-    }
-  }
+// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+//     if (stage === "build-html") {
+//       actions.setWebpackConfig({
+//         module: {
+//           rules: [
+//             {
+//               test: /locomotive-scroll/,
+//               use: loaders.null(),
+//             },
+//           ],
+//         },
+//       })
+//     }
+//   }
